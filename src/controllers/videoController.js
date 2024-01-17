6eb46ccebdb4fbb6c8d57e9b7b6f763aa4efdc3d
E-Han Song <iansong0714@gmail.com>
2024-01-17 16:27:50 +0900

    Handle JS file templates
    Using pug
    Learned url parameters, inheritance, extend ...

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,6 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const watch = (req, res) => res.send("Watch");
-export const edit = (req, res) => res.send("Edit");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
+export const search = (req, res) => res.send("Videos");
+export const deleteVideo = (req, res) => res.send("Delete Video");
+export const upload = (req, res) => res.send("Upload Video");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { deleteUser, edit } from "../controllers/userController";
+import { deleteUser, edit, logout, see } from "../controllers/userController";
 
 const userRouter = express.Router();
 
+userRouter.get(":id", see);
 userRouter.get("/edit", edit);
 userRouter.get("/delete", deleteUser);
+userRouter.get("/logout", logout);
 
 export default userRouter;
